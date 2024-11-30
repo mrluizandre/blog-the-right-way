d6af43939dce3d6a6fbdb338c8b91a42546b491a
Andre Costa <mr.luizandre@gmail.com>
2024-11-30 16:05:30 +0000

    add index page and component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 function Home() {
   return(
-    <>
-      <h1>Não clique em qualquer link que te mandem, vacilão!</h1>
-      <iframe src="https://giphy.com/embed/maNB0qAiRVAty" width="480" height="480" style={{}} frameBorder="0" className="giphy-embed" allowFullScreen>
-      </iframe>
-    </>
+      <h1>Olá, Mundo! De novo!</h1>
   )
 }
 
